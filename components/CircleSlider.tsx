d07eaa626a3cd92836642c506b5ddbf948db2cb4
Julie Cogley <snappy.9050@suou.waseda.jp>
2025-11-06 22:38:50 +0900

    shusei2:1106

--- components/CircleSlider.tsx
+++ components/CircleSlider.tsx
@@ -19,30 +19,26 @@ export default function CircleSlider() {
  [AutoScroll({ playOnInit: true,speed: 1.0, direction:'backward', stopOnInteraction: false,}),
  ],
  );
-
  return (
  <section>
  <div className="overflow-hidden" ref={emblaRef} >
  <div style={{display:"flex",gap:24}}>
- {[IMGS, ...IMGS, ...IMGS, ...IMGS, ...IMGS, ...IMGS, ...IMGS].map((src, i) => (
+ {[...IMGS, ...IMGS, ...IMGS, ...IMGS, ...IMGS, ...IMGS, ...IMGS].map((src, i) => (
  <div key={i} style={{flex:"0 0 auto",minWidth:0}}>
  <div style={{width:"375px" , height:"375px" ,margin:"0 16px"}}
  className="overflow-hidden">
  <Image
  src={src}
- alt=""
+ alt="image"
  width={400}
  height={400}
  className="w-full h-full object-cover"
  priority={i < 3}
  />
-
  </div>
  </div>
- 
  ))}
  </div>
- 
  </div>
  </section>
  );
