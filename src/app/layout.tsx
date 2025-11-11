9b35f6a8b11c789fb9d2f113ad30c29653f09e44
Julie Cogley <snappy.9050@suou.waseda.jp>
2025-11-12 08:44:49 +0900

    shusei1112

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,21 @@ display: "swap",
 
 export const metadata: Metadata = {
 title: "Corolla | やさしい花束を。",
-description: "ロスフラワーを活用したやさしいブーケをお届けします。",
+description: "ロスフラワーから作ったやさしいブーケをお届けします。",
+metadataBase: new URL("https://corolla-lp.vercel.app/"),
+openGraph:{
+title:"Corolla",
+description: "ロスフラワーから作ったやさしいブーケをお届けします。",
+url:"https://corolla-lp.vercel.app/",
+siteName:"Corolla",
+images:[{url: "/ogp-img.svg", width: 1200, height:630 }],
+locale:"ja_JP",
+type:"website",
+},
+twitter:{
+card:"summary_large_image",
+images:[ "/ogp-img.svg"],   
+}
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
